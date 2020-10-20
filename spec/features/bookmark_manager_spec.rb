b83c06447b86960fe18c955629c9b10d6bea63a8
Meryl Rastall <merylrastall@gmail.com>
2020-10-20 17:16:17 +0100

    refactored

--- spec/features/bookmark_manager_spec.rb
+++ spec/features/bookmark_manager_spec.rb
@@ -1,5 +1,6 @@
 require "capybara/rspec"
 require_relative "../../app/app"
+require "pg"
 
 Capybara.app = BookmarkManager
 
@@ -13,6 +14,12 @@ feature 'Getting bookmarks' do
 
   feature 'Viewing bookmarks' do
     scenario 'A user can see bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      
       visit('/bookmarks')
   
       expect(page).to have_content "http://www.makersacademy.com"
--- spec/unit/bookmarks_spec.rb
+++ spec/unit/bookmarks_spec.rb
@@ -1,9 +1,17 @@
 require_relative '../../app/bookmarks.rb'
 
 describe Bookmarks do
-  describe '#bookmarks' do
+  describe '.all' do
     it 'returns the bookmarks in the array' do
-      expect(Bookmarks.all).to eq ["http://www.makersacademy.com","http://google.com", "http://www.destroyallsoftware.com"]
-    end
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://google.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+    
+      bookmarks = Bookmarks.all
+      expect(bookmarks).to include('http://www.makersacademy.com')
+      expect(bookmarks).to include('http://www.destroyallsoftware.com')
+      expect(bookmarks).to include('http://google.com')    end
   end
 end
