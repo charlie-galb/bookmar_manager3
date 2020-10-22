567ed2071ca97af9bb5727d987755d6d046db840
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-22 16:34:31 +0100

    Debugging: Remove typos from Bookmarks class and tests

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -18,13 +18,13 @@ class Bookmarks
 
   def self.all
     result = @connection.exec "SELECT * FROM bookmarks"
-    result.map do |bookmark| 
-      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    result.map do |bookmark|
+      Bookmarks.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
     end
   end
 
   def self.create(url:, title:)
     result = @connection.exec("INSERT INTO bookmarks (url, Title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
-    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    Bookmarks.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
--- spec/units/bookmarks_spec.rb
+++ spec/units/bookmarks_spec.rb
@@ -5,14 +5,14 @@ describe Bookmarks do
     it 'displays a list bookmarks' do
       print 'setting up test database'
       connection = PG.connect(dbname: 'bookmark_manager_test')
-      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
-      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
-      Bookmark.create(url: "http://www.google.com", title: "Google")
-   
-      bookmarks = Bookmark.all
-   
+      bookmark = Bookmarks.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmarks.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmarks.create(url: "http://www.google.com", title: "Google")
+
+      bookmarks = Bookmarks.all
+
       expect(bookmarks.length).to eq 3
-      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first).to be_a Bookmarks
       expect(bookmarks.first.id).to eq bookmark.id
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
@@ -21,10 +21,10 @@ describe Bookmarks do
 
   describe '#create' do
     it 'creates a new bookmark' do
-      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      bookmark = Bookmarks.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
       persisted_data = PG.connect(dbname: 'bookmark_manager_test').query("SELECT * FROM bookmarks WHERE id = #{bookmark.id};")
 
-      expect(bookmark).to be_a Bookmark
+      expect(bookmark).to be_a Bookmarks
       expect(bookmark.id).to eq persisted_data.first['id']
       expect(bookmark.title).to eq 'Test Bookmark'
       expect(bookmark.url).to eq 'http://www.testbookmark.com'
